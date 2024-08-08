424447557b8b5023cde807ff9e2ec9fa64c8854f
Julia Lemos <77756159+JuliaLemos19@users.noreply.github.com>
2024-08-08 19:42:21 -0300

    Add files via upload

---
+++ OrdenaçãoeRecursividade.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+void insertionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+      
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    
+    std::vector<int> arr = {12, 11, 13, 5, 6};
+
+    std::cout << "Array original: ";
+    printArray(arr);
+
+    insertionSort(arr);
+
+    std::cout << "Array ordenado: ";
+    printArray(arr);
+
+    return 0;
+}
